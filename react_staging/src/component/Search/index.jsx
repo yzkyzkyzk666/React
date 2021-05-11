bc74349616cc8da5992fe6085df6d122c318557d
yzkyzkyzk666 <yangzhekong@gmail.com>
2021-05-12 01:02:49 +1000

    github search Component v1

--- react_staging/src/component/Search/index.jsx
+++ react_staging/src/component/Search/index.jsx
@@ -1,13 +1,28 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 
 export default class Search extends Component{
+
+    search = () =>{
+        // console.log(this.keyWordElement.value)
+        // const {value} = this.keyWordElement
+        const {keyWordElement:{value:keyWord}} = this
+        axios.get(`/api1/search/users?q=${keyWord}`).then(
+            response=>{
+                console.log('成功了',response.data)
+            },error=>{
+                console.warn('失败了！',error)
+            }
+        )
+    }
+
     render(){
         return(
             <section className="jumbotron">
                 <h3 className="jumbotron-heading">Search Github Users</h3>
                 <div>
-                    <input type="text" placeholder="enter the name you search"/>&nbsp;
-                    <button>Search</button>
+                    <input ref={c=>this.keyWordElement=c} type="text" placeholder="Enter the name you search"/>&nbsp;
+                    <button onClick={this.search}>Search</button>
                 </div>
             </section>
         )
