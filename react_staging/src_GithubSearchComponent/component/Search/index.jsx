8ff1eba537da25032dac6e13d2c2a120d5c12a22
yzkyzkyzk666 <yangzhekong@gmail.com>
2021-05-12 19:22:39 +1000

    github search Component Pubsub final version

--- react_staging/src_GithubSearchComponent/component/Search/index.jsx
+++ react_staging/src_GithubSearchComponent/component/Search/index.jsx
@@ -1,20 +1,21 @@
 import React, {Component} from 'react'
-import Pubsub from 'pubsub-js'
 import axios from 'axios'
 
 export default class Search extends Component{
 
     search = () =>{
+        // console.log(this.keyWordElement.value)
+        // const {value} = this.keyWordElement
         const {keyWordElement:{value:keyWord}} = this //获取用户的输入(连续解构赋值+重命名)
-        Pubsub.publish('yzk',{isFirst:false,isLoading:true})
-        // this.props.updateAppState({isFirst:false,isLoading:true})
+        this.props.updateAppState({isFirst:false,isLoading:true})
         axios.get(`/api1/search/users?q=${keyWord}`).then(
             response=>{
-                Pubsub.publish('yzk',{isLoading:false,users:response.data.items})
-                // this.props.updateAppState({isLoading:false,users:response.data.items})
+                // console.log('成功了',response.data)
+                // this.props.saveUsers(response.data.items)
+                this.props.updateAppState({isLoading:false,users:response.data.items})
             },error=>{
-                Pubsub.publish('yzk',{isLoading:false,err:error.message})
-                // this.props.updateAppState({isLoading:false,err:error.message})
+                // console.warn('失败了！',error)
+                this.props.updateAppState({isLoading:false,err:error.message})
             }
         )
     }
