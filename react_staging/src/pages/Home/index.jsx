82d812549d5ab5fc6c9641bbeeacf48eadf6d2c0
yzkyzkyzk666 <yangzhekong@gmail.com>
2021-05-16 14:31:04 +1000

    Nested Router嵌套路由

--- react_staging/src/pages/Home/index.jsx
+++ react_staging/src/pages/Home/index.jsx
@@ -1,9 +1,33 @@
 import React, { Component } from 'react'
+import MyNavLink from '../../components/MyNavLink'
+import {Route,Switch,Redirect} from 'react-router-dom'
+import News from './News'
+import Message from './Message'
 
 export default class Home extends Component {
 	render() {
 		return (
-			<h3>我是Home的内容</h3>
+			<div>
+				<h2>Home组件内容</h2>
+				<div>
+					<ul className="nav nav-tabs">
+						<li>
+							{/*<a className="list-group-item" href="./home-news.html">News</a>*/}
+							<MyNavLink to="/home/news">News</MyNavLink>
+						</li>
+						<li>
+							{/*<a className="list-group-item active" href="./home-message.html">Message</a>*/}
+							<MyNavLink to="/home/message">Message</MyNavLink>
+						</li>
+					</ul>
+					{/*	注册路由 */}
+					<Switch>
+						<Route path='/home/news' component={News}/>
+						<Route path="/home/message" component={Message}/>
+						<Redirect to="/home/news"/>
+					</Switch>
+				</div>
+			</div>
 		)
 	}
 }
