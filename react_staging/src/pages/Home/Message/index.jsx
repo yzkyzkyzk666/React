cbeb9cdf8714f7c138a91b5a1c3595ac81e3e4d6
yzkyzkyzk666 <yangzhekong@gmail.com>
2021-05-16 16:07:31 +1000

    params传递 v1

--- react_staging/src/pages/Home/Message/index.jsx
+++ react_staging/src/pages/Home/Message/index.jsx
@@ -1,20 +1,36 @@
 import React,{Component} from 'react'
+import {Link,Route,Switch,Redirect} from 'react-router-dom'
+import Detail from './Detail'
 
 export default class Message extends Component{
+    state={
+        messageArr:[
+            {id:'01',title:'message1'},
+            {id:'02',title:'message2'},
+            {id:'03',title:'message3'}
+        ]
+    }
     render(){
+        const {messageArr} = this.state
         return(
             <div>
                 <ul>
-                    <li>
-                        <a href="/message1">message001</a>&nbsp;&nbsp;
-                    </li>
-                    <li>
-                        <a href="/message2">message002</a>&nbsp;&nbsp;
-                    </li>
-                    <li>
-                        <a href="/message/3">message003</a>&nbsp;&nbsp;
-                    </li>
+                    {
+                        messageArr.map((messageObj)=>{
+                            return(
+                                <li key={messageObj.id}>
+                                    {/*向路由组件传递params参数*/}
+                                    <Link to={`/home/message/detail/${messageObj.id}/${messageObj.title}`}>{messageObj.title}</Link>&nbsp;&nbsp;
+                                </li>
+                            )
+                        })
+                    }
+                {/*map外侧的{}别忘记写，忘记好多次了...*/}
                 </ul>
+                <hr/>
+                {/*路由组件声明接收params参数*/}
+                <Route path="/home/message/detail/:id/:title" component={Detail}/>
+                {/*<Redirect to="/home/message/detail/:id/:title" />*/}
             </div>
         )
     }
