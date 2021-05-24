064c02608ea5c8f72137bbf0be21db1d0686ae25
yzkyzkyzk666 <yangzhekong@gmail.com>
2021-05-25 00:13:53 +1000

    react-redux Person+Count devtools

--- redux_test/src/redux/store.js
+++ redux_test/src/redux/store.js
@@ -2,9 +2,10 @@ import {createStore,applyMiddleware,combineReducers} from 'redux'
 import countReducer from './reducers/count'
 import personReducer from './reducers/person'
 import thunk from 'redux-thunk'
+import {composeWithDevTools} from "redux-devtools-extension";
 
 const allReducers = combineReducers({
     count:countReducer,
     people:personReducer
 })
-export default createStore(allReducers,applyMiddleware(thunk))
+export default createStore(allReducers,composeWithDevTools(applyMiddleware(thunk)))
